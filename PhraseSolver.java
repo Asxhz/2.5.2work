6d67793d4cafccad5a36b57dfd973cdf50d8f712
Ash <69103680+Asxhz@users.noreply.github.com>
2023-10-26 11:46:51 -0700

    Add files via upload

---
+++ PhraseSolver.java
@@ -0,0 +1,50 @@
+/*
+ * Activity 2.5.2
+ *
+ *  The PhraseSolver class the PhraseSolverGame
+ */
+import java.util.Scanner;
+  
+public class PhraseSolver
+{
+  /* your code here - attributes */
+  private String player1;
+  private String player2;
+  private String board;
+  private Boolean solved;
+
+  /* your code here - constructor(s) */ 
+  public PhraseSolver()
+  {
+    player1 = null;
+    player2 = null;
+    board = null;
+    solved = false;
+
+  }
+
+  /* your code here - accessor(s) */
+  
+  /* your code here - mutator(s)  */
+
+  public void play()
+  {
+    boolean solved = false;
+    int currentPlayer = 1;
+
+    Scanner input = new Scanner(System.in);
+    
+    boolean correct = true;
+    while (!solved) 
+    {
+      
+      /* your code here - game logic */
+      
+      
+      /* your code here - determine how game ends */
+      solved = true; 
+    } 
+   
+  }
+  
+}
